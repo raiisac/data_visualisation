94bf93f457fd1d168942f731db3f12736475ac13
StevenHuygens <stevenhuygens13@gmail.com>
2024-04-30 18:56:01 +0200

    first test commit

--- sveltekit-app/src/routes/app2/+page.svelte
+++ sveltekit-app/src/routes/app2/+page.svelte
@@ -14,8 +14,6 @@
 	<pre>npm create svelte@latest</pre>
 
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		test
 	</p>
 </div>
