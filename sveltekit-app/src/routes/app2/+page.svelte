c72423c3e062cc2dfd790dfc5911b9009b399e90
StevenHuygens <stevenhuygens13@gmail.com>
2024-05-01 21:17:59 +0200

    working map example

--- sveltekit-app/src/routes/app2/+page.svelte
+++ sveltekit-app/src/routes/app2/+page.svelte
@@ -1,27 +1,65 @@
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
-
 <script>
-    export let data = [];
+	import { json, geoTransform, geoPath } from 'd3';
 
-	let address_to_coordinates_map = new Map();
-	for (let i = 0; i < data.coordinates.length; i++) {
-		address_to_coordinates_map.set(data.coordinates[i].address, [data.coordinates[i].latitude, data.coordinates[i].longitude]);
-	}
-	let address_to_coordinates_map_keys = Array.from(address_to_coordinates_map.keys());
+	let width, height;
+
+	let geojsonPath = "http://localhost:5173/europe.geojson"
+	let geojson;
+	json(geojsonPath).then((data) => geojson = data);
+
+	const minLon = -24.542225;
+	const maxLon = 46.710815;
+	const minLat = 31.78639;
+	const maxLat = 66.536102;
+	
+	$: rescale_lon = function(longitude) {
+		let range_min = 0;
+		let range_max = width;
+      	return ((range_max - range_min)*(longitude-minLon))/(maxLon-minLon) + range_min
+  	}
+
+	$: rescale_lat = function(latitude) {
+		let range_min = height;
+		let range_max = 0;
+      	return ((range_max - range_min)*(latitude-minLat))/(maxLat-minLat) + range_min
+  	}
+
+	$: projection = geoTransform({
+      point: function(x, y) {
+          this.stream.point(rescale_lon(x), rescale_lat(y));
+      }
+    });
+	$: pathGenerator = geoPath(projection);
+
+	let countries = []
+	$: if (geojson) countries = geojson.features.map(feature => {
+		return {
+			...feature,
+			path: pathGenerator(feature)
+		};
+	});
+
+	$: console.log(countries);
+
+	
 </script>
 
-<h1>Employees</h1>
+<main
+	bind:clientWidth={width}
+	bind:clientHeight={height}
+>
+	<svg width={width} height={height}>
+		{#each countries as country}
+			<path d={country.path}/>
+		{/each}
+	</svg>
+</main>
 
-<ul>
-{#each data.plants as plant}
-    <li>{plant.PlantKey} {plant.Plant}</li>
-{/each}
 
-{#each address_to_coordinates_map_keys as key}
-    <li>{key} {address_to_coordinates_map.get(key)}</li>
-{/each}
-</ul>
+<style>
+	main {
+		width: 100vw;
+		height: 100vh;
+		overflow: hidden;
+	}
+</style>
